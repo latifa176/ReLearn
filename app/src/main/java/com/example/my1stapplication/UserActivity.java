2f4093394d47b97b8e803cb3575c14903a8e1668
Raghad <RaghadA.Alkhudhair@gmail.com>
2019-10-07 12:53:38 +0300

    raghad commit(everything works except bookslist and addpost will need modifications)

--- app/src/main/java/com/example/my1stapplication/UserActivity.java
+++ app/src/main/java/com/example/my1stapplication/UserActivity.java
@@ -5,21 +5,41 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class UserActivity extends AppCompatActivity {
     Button btnLogOut;
+    Button addPost;
+    Button bookslist;
     FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
+    StorageReference courseReference, materialTypeReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
         btnLogOut = (Button) findViewById(R.id.btnLogOut);
+        addPost = (Button) findViewById(R.id.b1);
+        bookslist = (Button) findViewById(R.id.button);
+
+        bookslist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                movetobookslist();
+            }
+        });
+
+        addPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToaddPost();
+            }
+        });
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,5 +52,62 @@ public class UserActivity extends AppCompatActivity {
             }
         });
 
+
+        courseReference = FirebaseStorage.getInstance().getReference().child("posts").child("coursename");
+        materialTypeReference = FirebaseStorage.getInstance().getReference().child("posts").child("materialtype");
     }
-}
+        private void moveToaddPost () {
+            Intent intent = new Intent(UserActivity.this, test.class);
+            startActivity(intent);
+
+
+        }
+
+
+        private void movetobookslist () {
+            Intent intent = new Intent(UserActivity.this, BooksList.class);
+            startActivity(intent);
+
+        }
+
+
+
+    /*@Override
+    public void onStart() {
+        super.onStart();
+
+        // Add value event listener to the post
+        // [START post_value_event_listener]
+        ChildEventListener postListener = new ChildEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                Post post = dataSnapshot.getValue(Post.class);
+                // [START_EXCLUDE]
+                mAuthorView.setText(post.author);
+                mTitleView.setText(post.title);
+                mBodyView.setText(post.body);
+                // [END_EXCLUDE]
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                // [START_EXCLUDE]
+                Toast.makeText(PostDetailActivity.this, "Failed to load post.",
+                        Toast.LENGTH_SHORT).show();
+                // [END_EXCLUDE]
+            }
+        };
+        mPostReference.addValueEventListener(postListener);
+        // [END post_value_event_listener]
+
+        // Keep copy of post listener so we can remove it when app stops
+        mPostListener = postListener;
+
+        // Listen for comments
+        mAdapter = new CommentAdapter(this, mCommentsReference);
+        mCommentsRecycler.setAdapter(mAdapter);
+    }*/
+    }
